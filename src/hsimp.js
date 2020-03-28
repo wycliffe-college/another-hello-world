14459387e2ac76844825d27eaaf3d9b8ef59fb2b
brend <indb@wycliffe.co.uk>
2020-03-28 21:14:24 +0000

    Added password checker cos i got boreddddd

---
+++ src/hsimp.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var main = require("hsimp-main/bootstrap")(require("hsimp-main"));
+var L = require("hsimp-library");
+
+// define hsimp
+var hsimp = L.curry(function (options, input) {
+    if (!input || !L.isFunction(input.click)) {
+        throw new Error("input not supplied");
+    }
+
+    var classes = input.getAttribute("class") + " hsimp-level";
+    var outputTime = L.isFunction(options.outputTime) ? options.outputTime : L.noOp;
+    var outputChecks = L.isFunction(options.outputChecks) ? options.outputChecks : L.noOp;
+    var hsimpOptions = L.isObject(options.options) ? options.options : {};
+
+    input.setAttribute("class", classes);
+
+    main.setOptions(hsimpOptions);
+
+    input.addEventListener("keyup", function () {
+        var password = main(input.value);
+
+        if (input.value.length) {
+            input.setAttribute("class", classes + " hsimp-level--" + password.getSecurityLevel());
+            outputTime(password.getString(), input);
+            outputChecks(password.getChecks(), input);
+        } else {
+            input.setAttribute("class", classes);
+            outputTime(false, input);
+            outputChecks([]);
+        }
+    });
+});
+
+// see https://github.com/howsecureismypassword/modules-main/blob/develop/dictionary.json
+hsimp.setDictionary = main.setDictionary; 
+
+// see https://github.com/howsecureismypassword/modules-period/blob/develop/period-dictionary.json
+hsimp.setPeriodDictionary = main.setPeriodDictionary;
+
+// see https://github.com/howsecureismypassword/modules-named-number/blob/develop/named-number-dictionary.json
+hsimp.setNamedNumberDictionary = main.setNamedNumberDictionary 
+
+// see https://github.com/howsecureismypassword/modules-checker/blob/develop/checker-dictionary.json
+hsimp.setCheckerDictionary = main.setCheckerDictionary;
+
+module.exports = hsimp;
